f49de1a915794cea8a8adb5be0ace1ba0d66719c
Malhaar-k <malhaar.karandikar@gmail.com>
2024-06-19 22:45:00 +0100

    removed compiled binary tracking

---
+++ dash_display/run_test.py
@@ -0,0 +1,9 @@
+#Create a tkinter window that displays one string of text
+import tkinter as tk
+
+
+# Create the main window with size 200x200
+window = tk.Tk(baseName= "CAN Dashboard")
+window.geometry("500x500")
+window.title("CAN Dashboard")
+window.mainloop()
